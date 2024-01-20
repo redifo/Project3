ee2d12e1807ff034d26058d09730e135abb8afd5
Redifo <kaanredif@gmail.com>
2024-01-21 00:32:24 +0100

    Pep8 corrections to the run.py and update test accordigly

--- TEST.md
+++ TEST.md
@@ -10,3 +10,88 @@
 | Input Error Handling       | Display error for invalid inputs and allows correction            | Entered invalid inputs and corrected               | Error displayed, correction allowed      | Pass      |
 | Data Persistence           | Scores and words persist across sessions                          | Restarted game, checked scores and word list       | Data persisted correctly                 | Pass      |
 | User Interface Consistency | UI elements consistent and responsive throughout gameplay         | Interacted with various UI elements                | UI consistent and responsive             | Pass      |
+
+
+Initial results from pep8 checker:
+21: W605 invalid escape sequence '\ '
+21: W605 invalid escape sequence '\ '
+22: W605 invalid escape sequence '\ '
+22: W605 invalid escape sequence '\ '
+22: W605 invalid escape sequence '\ '
+22: W605 invalid escape sequence '\ '
+22: W605 invalid escape sequence '\ '
+23: W605 invalid escape sequence '\ '
+24: W605 invalid escape sequence '\_'
+24: W605 invalid escape sequence '\_'
+24: W605 invalid escape sequence '\_'
+24: W605 invalid escape sequence '\_'
+24: W605 invalid escape sequence '\_'
+84: E501 line too long (88 > 79 characters)
+86: W291 trailing whitespace
+88: W291 trailing whitespace
+90: W293 blank line contains whitespace
+95: E501 line too long (94 > 79 characters)
+95: W291 trailing whitespace
+96: W293 blank line contains whitespace
+99: E231 missing whitespace after ','
+99: E231 missing whitespace after ','
+99: E501 line too long (112 > 79 characters)
+99: E231 missing whitespace after ','
+100: E501 line too long (139 > 79 characters)
+105: E501 line too long (121 > 79 characters)
+110: E501 line too long (81 > 79 characters)
+113: E501 line too long (97 > 79 characters)
+115: E225 missing whitespace around operator
+118: E225 missing whitespace around operator
+119: E225 missing whitespace around operator
+124: W293 blank line contains whitespace
+126: E303 too many blank lines (2)
+128: W291 trailing whitespace
+135: W293 blank line contains whitespace
+138: W293 blank line contains whitespace
+145: W293 blank line contains whitespace
+146: W293 blank line contains whitespace
+147: E303 too many blank lines (2)
+149: W291 trailing whitespace
+156: E501 line too long (139 > 79 characters)
+158: W293 blank line contains whitespace
+166: E501 line too long (127 > 79 characters)
+167: E501 line too long (116 > 79 characters)
+168: E501 line too long (144 > 79 characters)
+169: E501 line too long (122 > 79 characters)
+170: E501 line too long (188 > 79 characters)
+171: E501 line too long (110 > 79 characters)
+188: E501 line too long (112 > 79 characters)
+191: E501 line too long (113 > 79 characters)
+194: E501 line too long (111 > 79 characters)
+208: W293 blank line contains whitespace
+213: E501 line too long (97 > 79 characters)
+216: E261 at least two spaces before inline comment
+216: E262 inline comment should start with '# '
+221: E225 missing whitespace around operator
+229: E501 line too long (106 > 79 characters)
+230: W293 blank line contains whitespace
+231: W293 blank line contains whitespace
+232: E303 too many blank lines (2)
+238: W293 blank line contains whitespace
+242: E501 line too long (126 > 79 characters)
+253: W293 blank line contains whitespace
+254: E302 expected 2 blank lines, found 1
+262: W291 trailing whitespace
+263: E501 line too long (184 > 79 characters)
+269: W293 blank line contains whitespace
+270: E302 expected 2 blank lines, found 1
+274: E265 block comment should start with '# '
+276: E265 block comment should start with '# '
+276: E501 line too long (83 > 79 characters)
+277: E225 missing whitespace around operator
+279: W293 blank line contains whitespace
+282: E225 missing whitespace around operator
+283: W293 blank line contains whitespace
+284: E225 missing whitespace around operator
+284: E261 at least two spaces before inline comment
+287: W293 blank line contains whitespace
+292: W293 blank line contains whitespace
+297: W293 blank line contains whitespace
+302: E305 expected 2 blank lines after class or function definition, found 1
+303: W292 no newline at end of file
--- run.py
+++ run.py
@@ -16,27 +16,28 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('project3-ci')
 
-title="""             __        __                     _   _        
-             \ \      / /   ___    _ __    __| | | |   ___ 
+title = """             __        __                     _   _
+             \ \      / /   ___    _ __    __| | | |   ___
               \ \ /\ / /   / _ \  | '__|  / _` | | |  / _ \.
                \ V  V /   | (_) | | |    | (_| | | | |  __/
                 \_/\_/     \___/  |_|     \__,_| |_|  \___|
         """
 
+
 class WordleGame:
     def __init__(self, sheet):
-        #sheet with 14885 5 letter words
+        # sheet with 14885 5 letter words
         self.answers_sheet = sheet.worksheet('answer')
-        #sheet with 2309 5 letter words
+        # sheet with 2309 5 letter words
         self.words_sheet = sheet.worksheet('words')
         self.all_answers = self.answers_sheet.get_all_values()
         self.all_words = self.words_sheet.get_all_values()
         self.highscores_sheet = sheet.worksheet('highscores')
-        self.highscores_data= self.highscores_sheet.get_all_values()
+        self.highscores_data = self.highscores_sheet.get_all_values()
         self.player_name = None
         self.difficulty_choice = None
         self.number_of_guesses = 0
-        self.guessed_correctly = False
+        self.guessed = False
 
     def run_game(self):
         """
@@ -46,113 +47,117 @@ class WordleGame:
         Calls the the end game function when the game ens
         """
 
-        if self.player_name == None:
+        if self.player_name is None:
             self.player_name = self.get_player_name()
         self.difficulty_choice = self.select_difficulty()
         difficulty_guess_mapping = {'easy': 10, 'normal': 6, 'hard': 4}
         print(f"Hello, {self.player_name}!")
         input("Press Enter to start the game")
-        
-        #had to iterate due to being a list nested in a list
+        # had to iterate due to being a list nested in a list
         all_words_list = [' '.join(sublist) for sublist in self.all_words]
-        #pick a random word from the answers list
-        answer=random.choice(self.all_answers)
-        answer_string= answer[0]
-        
-        #start timer
+        # pick a random word from the answers list
+        answer = random.choice(self.all_answers)
+        answer_string = answer[0]
+        # start timer
         start_time = time.time()
         self.clear_terminal()
-        previous_guesses={}
+        previous_guesses = {}
 
-        #Run the game as long as the number of guesses does not exceed the chosen difficulty limit and the word is not guessed correctly
-        while self.number_of_guesses < difficulty_guess_mapping[self.difficulty_choice] and not self.guessed_correctly:
+        # Run the game as long as the number of guesses does not exceed the
+        # chosen difficulty limit and the word is not guessed correctly
+        while self.number_of_guesses+1 < difficulty_guess_mapping[
+                self.difficulty_choice] and not self.guessed:
             self.clear_terminal()
             print(title)
-            
-            current_answer_display = ['_' for i in range(5)] 
+            current_answer_display = ['_' for i in range(5)]
             print("Your previous guesses and their clues:")
             for guess, clue in previous_guesses.items():
-                print(f"{guess}: {' '.join(clue)}")  
-            
+                print(f"{guess}: {' '.join(clue)}")
+
             while True:
                 guess = input("Input a 5-letter word and press enter:")
                 try:
-                    #if the guess is in the full list of allowed words 
-                    if guess in all_words_list: 
-                        self.guessed_correctly, updated_display = self.process_guess(answer_string, guess, current_answer_display)
-                        #equating the number of items in the dictionary to the ensures that 
-                        #the number of gueses does not increase if the user inputs the same word over and over again.
-                        self.number_of_guesses = len(previous_guesses) 
+                    # if the guess is in the full list of allowed words
+                    if guess in all_words_list:
+                        self.guessed, updated_display = self.process_guess(
+                            answer_string, guess, current_answer_display)
+                        # equating the number of items in the dictionary to
+                        # ensures that the number of gueses does not increase
+                        # if the user inputs the same word over and over again.
+                        self.number_of_guesses = len(previous_guesses)
                         previous_guesses[guess] = updated_display
-           
                         break  # Break the loop if guess is valid
                     else:
                         raise ValueError("Invalid word")
                 except ValueError as e:
-                    print(f"\r{e}:Please try again with a valid word that is 5 letters long.")     
-        
+                    print(f"\r{e}: Please try again with a valid", end="")
+                    print("word that is 5 letters long.")
         elapsed_time = time.time() - start_time
-        if self.guessed_correctly:
-            self.add_highscore(self.player_name,self.difficulty_choice,self.number_of_guesses,int(elapsed_time))
-            print(f"Your final time is {elapsed_time:.0f} seconds. It took you {self.number_of_guesses+1} guesses to find the right word!")
+        if self.guessed:
+            self.add_highscore(self.player_name,
+                               self.difficulty_choice, self.number_of_guesses,
+                               int(elapsed_time))
+            print(f"Your final time is {elapsed_time: .0f} seconds.It", end="")
+            print(f" took you {self.number_of_guesses+1} guesses", end="")
+            print("to find the right word!")
             self.end_game()
 
         else:
             print("_"*80)
-            print(f"You could not guess the word in the given amount of guesses, the correct answer was {answer_string}")
+            print(f"You could not guess the word in the given ", end="")
+            print(f"amount of guesses, the correct answer was {answer_string}")
             self.end_game()
 
     def end_game(self):
         """
-        Ends the game and gives options to play again or return to the main menu.
+        Ends the game and gives options to play again or
+        return to the main menu.
         """
         while True:
-            end_game_input = input("Enter '1' to play again or '2' to return to the main menu: ")
+            end_game_input = input(
+                "Enter '1' to play again or '2' to return to the main menu: ")
             if end_game_input == '1':
-                self.guessed_correctly=False
+                self.guessed = False
                 self.run_game()
             elif end_game_input == '2':
-                self.guessed_correctly=False
-                self.player_name=None
+                self.guessed = False
+                self.player_name = None
                 print_menu(firstload=True)
                 break  # Break out of the loop when a valid option is entered
             else:
                 print("Please enter a valid option (1 or 2).")
-            
 
     def process_guess(self, answer, guess, answer_display):
         """
-        Check if the guessed word is correct or not and 
+        Check if the guessed word is correct or not and
         if not true give clues depending the letter locations
         """
         if guess == answer:
             print("Congratulations! You guessed the word correctly.")
-            self.guessed_correctly = True
+            self.guessed = True
         else:
-            
             for i, letter in enumerate(guess):
                 if letter == answer[i]:
-                    
                     answer_display[i] = letter
                 elif letter in answer:
                     answer_display[i] = letter+"*"
                 else:
                     answer_display[i] = "_"
-        return self.guessed_correctly, answer_display
-        
-        
+        return self.guessed, answer_display
+
     def get_player_name(self):
         """
-        Gets the player name and checks its length 
+        Gets the player name and checks its length
         """
         while True:
             name = input("Please enter your name: ").strip()
             if len(name) > 2 and name.isalnum():
                 return name.title()
             else:
-                print("\rYour name must be at least 3 characters long and can only contain letters and numbers. Please try again.", end="")
-                print("\n")
-                
+                print(
+                    "\rYour name must be at least 3 characters long ", end="")
+                print("and can only contain letters/numbers. Try again.")
+
     def how_to_play(self):
         """
         Display how to play information
@@ -160,14 +165,32 @@ class WordleGame:
         print(title)
         print("-" * 80)
         print("How to Play Wordle:")
-        print("1. You will be asked to enter your name upon starting the game. Once your name is entered, the game will begin")
-        print("2. You'll be asked to guess a 5-letter word. The game will provide feedback on your guess attempts.")
-        print("3. The feedback consists of two elements: correct letters in the correct position and correct letters in the wrong position (*)")
-        print("4. For example, if the secret word is 'APPLE' and you guess is 'ADOPT' the feedback might look like this:")
-        print("   A _ _ P*_ as you can see the 'A' is in correct position with no marking and P is with an asterix idicating the letter is present in the word but in a different location")
-        print("5. If you correctly guess the word within the specified number of attempts, you win the game!")
-        print("6. You can choose to exit the game at any time")
-        print("7. Have fun!")
+        print(
+            "1. You will be asked to enter your name upon starting the game."
+        )
+        print("2. Once your name is entered, the game will begin")
+        print(
+            "3. You'll be asked to type and try to guess a 5-letter word."
+        )
+        print("4. The game will provide feedback on your guess attempts.")
+        print(
+            "5. The feedback consists of two elements: "
+        )
+        print("correct letters and correct letters in the wrong position (*)")
+        print(
+            "6. For example, if the answer is 'APPLE' and you guess is 'ADOPT'"
+        )
+        print("the feedback might look like this: A _ _ P*_ ")
+        print(
+            "as you can see the 'A' is in correct position with no marking "
+        )
+        print(
+            "P* idicating the letter is in the answer at a different location"
+        )
+        print(
+            "7. If you correctly guess the word, you win the game!"
+        )
+        print("8. Have fun!")
         input("Press Enter to return to the main menu...")
 
     def select_difficulty(self):
@@ -182,13 +205,16 @@ class WordleGame:
             difficulty_choice = input("Enter choice (e/n/h): ").lower()
             if difficulty_choice in {"e", "n", "h"}:
                 if difficulty_choice == "e":
-                    print("You have selected easy difficulty. You will be given 10 attempts to guess the word.")
+                    print("You have selected easy difficulty. ")
+                    print("You will be given 10 attempts to guess the word.")
                     return "easy"
                 elif difficulty_choice == "n":
-                    print("You have selected normal difficulty. You will be given 6 attempts to guess the word.")
+                    print("You have selected normal difficulty.")
+                    print("You will be given 6 attempts to guess the word.")
                     return "normal"
                 elif difficulty_choice == "h":
-                    print("You have selected hard difficulty. You will be given 4 attempts to guess the word.")
+                    print("You have selected hard difficulty.")
+                    print("You will be given 4 attempts to guess the word.")
                     return "hard"
                 break
             else:
@@ -202,20 +228,21 @@ class WordleGame:
             return 6
         elif self.difficulty_choice == "hard":
             return 4
-        
+
     def clear_terminal(self):
         """
         Clears terminal
         """
-        # recovered from https://stackoverflow.com/questions/2084508/clear-the-terminal-in-python
+        # recovered from
+        # https://stackoverflow.com/questions/2084508/clear-the-terminal-in-python
         os.system('cls' if os.name == 'nt' else 'clear')
 
-    def show_highscores(self):#change this code
+    def show_highscores(self):
         """
         Show highscores data of the best 5 players
         The data is take from google sheets
         """
-        self.highscores_data= self.highscores_sheet.get_all_values()
+        self.highscores_data = self.highscores_sheet.get_all_values()
         print(title)
         print("-" * 80)
         print("Top 10 Highscores:")
@@ -223,21 +250,21 @@ class WordleGame:
         # Map difficulty levels to numerical values for sorting
         difficulty_mapping = {'easy': 1, 'normal': 2, 'hard': 3}
         # Skip the header row and convert numeric values for sorting
-        highscores_data = [[row[0], row[1], int(row[2]), int(row[3])] for row in self.highscores_data[1:]]
-        
-        
+        highscores_data = [[row[0], row[1], int(row[2]),
+                            int(row[3])] for row in self.highscores_data[1:]]
+
         def sort_scores(score):
             """
             Sorting function
             """
             name, difficulty, guesses, time = score
             return (guesses, difficulty_mapping[difficulty], time)
-        
+
         # Sort the high_scores list
         sorted_highscores = sorted(highscores_data, key=sort_scores)
         for i, score in enumerate(sorted_highscores[:10]):
-            print(f"{i + 1}. Name: {score[0]}, Difficulty: {score[1].title()}, Guesses: {score[2]}, Time: {score[3]} seconds")
-
+            print(f"{i + 1}. Name: {score[0]}, Difficulty:", end="")
+            print(f"{score[1].title()}, Guesses: {score[2]}, Time: {score[3]}")
         print("-" * 80)
         input("Press Enter to return to the main menu...")
 
@@ -247,7 +274,8 @@ class WordleGame:
         """
         new_highscore = [player_name, difficulty, num_guesses, time_taken]
         self.highscores_sheet.append_row(new_highscore)
-        
+
+
 def print_menu(firstload):
     """
     Function to display the main menu options and the ascii wordle title.
@@ -256,45 +284,48 @@ def print_menu(firstload):
     if firstload:
         os.system('cls' if os.name == 'nt' else 'clear')
         print(title)
-        print("-" * 80) 
-        print(" Welcome to our Wordle Game! Get ready to test your \n word-guessing skills and have some fun. Let's see if you can crack the \n code and climb up the highscore chart!")
+        print("-" * 80)
+        print(" Welcome to our Wordle Game! Get ready to test your")
+        print(" word-guessing skills and have some fun. Let's see if")
+        print(" you can crack the code and climb up the highscore chart!")
         print("-" * 80)
         print("1. Start Game")
         print("2. Show High-Scores")
         print("3. How to play")
         print("-" * 80)
-    
+
+
 def main():
     """
     The main function to run the Wordle gamemeu indefinitly.
     """
-    #create an instance of the WordleGame class named game
+    # Create an instance of the WordleGame class named game
     game = WordleGame(SHEET)
-    #Set the flag of print_menu fuction to true for initally showing the whole menu
-    firstload=True
+    # Set the flag of print_menu fuction to true for only initally showing menu
+    firstload = True
     while True:
-        
+
         # Display the menu and get the user's choice
         print_menu(firstload)
-        choice=input("Select an option 1 2 or 3: ")
-        
-        firstload= False # Set the flag to False after the first display
+        choice = input("Select an option 1 2 or 3: ")
+        firstload = False  # Set the flag to False after the first display
         if choice == "1":
             game.run_game()
-            
+
         elif choice == "2":
             game.clear_terminal()
             game.show_highscores()
             firstload = True
-            
+
         elif choice == "3":
             game.clear_terminal()
             game.how_to_play()
             firstload = True
-            
+
         else:
             print("Invalid option. Please choose a valid option.")
             continue
+
 
 if __name__ == "__main__":
     main()
